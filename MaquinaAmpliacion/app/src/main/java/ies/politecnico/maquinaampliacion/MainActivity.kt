6f4d6d17ec85ca79c74e7fd49ba17aaa02b2f1d0
AlbertoCodex <zenycs3@gmail.com>
2024-11-07 21:26:05 +0100

    Add fragments and navigation

--- MaquinaAmpliacion/app/src/main/java/ies/politecnico/maquinaampliacion/MainActivity.kt
+++ MaquinaAmpliacion/app/src/main/java/ies/politecnico/maquinaampliacion/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ies.politecnico.maquinaampliacion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Late-initialized variables for binding and navigation controller
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    // onCreate method override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Inflate the layout using view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Find the NavHostFragment and get its NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_activity_nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Get reference to the BottomNavigationView from the layout
+        val navView: BottomNavigationView = binding.bottomNavView
+
+        // Set item icon tint list to null (optional, to disable default icon tinting)
+        navView.itemIconTintList = null
+
+        // Set up the BottomNavigationView with the NavController for navigation
+        navView.setupWithNavController(navController)
     }
 }
