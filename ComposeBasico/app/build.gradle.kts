8483207b76a1c523aa672b9423845711cdf8d418
AlbertoCodex <alberto13nn@gmail.com>
2024-11-20 00:56:07 +0100

    Codigo Composable & Previews

--- ComposeBasico/app/build.gradle.kts
+++ ComposeBasico/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ies.politecnico.composebasico"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ies.politecnico.composebasico"
